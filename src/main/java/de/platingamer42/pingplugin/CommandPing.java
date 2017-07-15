9f319c0399f869114943e93e723edbc35364f6cc
BuildTools <unconfigured@null.spigotmc.org>
2017-07-15 19:10:37 +0200

    finished. Denk ich.

--- src/main/java/de/platingamer42/pingplugin/CommandPing.java
+++ src/main/java/de/platingamer42/pingplugin/CommandPing.java
@@ -1,18 +1,24 @@
 package de.platingamer42.pingplugin;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class CommandPing extends Command {
-    public CommandPing() {
+    CommandPing() {
         super("ping");
     }
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof ProxiedPlayer)){
+            sender.sendMessage(new ComponentBuilder("Du musst ein Spieler sein," +
+                    " um diesen Befehl ausführen zu künnen").color(ChatColor.RED).create());
+            return;
+        }
         ProxiedPlayer p = (ProxiedPlayer) sender;
-        String s = String.format("Dein derzeitiger Ping ist: %sms", p.getPing());
-        sender.sendMessage(new ComponentBuilder(s).create());
+        String s = String.format("Dein derzeitiger Ping ist: %s%sms", ChatColor.GREEN, p.getPing());
+        sender.sendMessage(new ComponentBuilder(s).color(ChatColor.AQUA).create());
     }
 }
